da9f95e6af13c6e5fe96d5fe676511eb26239e53
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-06-27 23:38:56 +0100

    display posts

--- src/app/components/all-posts/all-posts.component.ts
+++ src/app/components/all-posts/all-posts.component.ts
@@ -3,11 +3,13 @@ import { NetworkService } from '../../services/network.service';
 import { Store } from '@ngrx/store';
 import { PostApiActions } from '../../state/actions/posts.actions';
 import { Post } from '../../state/models/post';
+import { selectPosts } from '../../state/selectors/posts.selectors';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-all-posts',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './all-posts.component.html',
   styleUrl: './all-posts.component.scss',
   providers: [NetworkService]
@@ -15,6 +17,8 @@ import { Post } from '../../state/models/post';
 export class AllPostsComponent implements OnInit {
 
   constructor(private __network: NetworkService, private store: Store) { }
+
+  $posts = this.store.select(selectPosts);
 
   ngOnInit() {
     this.__network
---
+++ src/app/components/post-id/post-id.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-id',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-id.component.html',
+  styleUrl: './post-id.component.scss'
+})
+export class PostIdComponent {
+
+}
