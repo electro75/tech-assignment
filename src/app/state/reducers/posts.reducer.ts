05ff9f01afeb4eceaec35fc6ab5a7403aa1404e2
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-06-27 23:38:56 +0100

    display posts

--- src/app/state/reducers/posts.reducer.ts
+++ src/app/state/reducers/posts.reducer.ts
@@ -3,14 +3,11 @@ import { PostApiActions } from '../actions/posts.actions';
 import { Post } from '../models/post';
 
 
-
-export const initialState: { posts: Post[] } = {
-    posts: [],
-};
+export const initialState: Post[] = [];
 
 export const postsReducer = createReducer(
     initialState,
-    on(PostApiActions.retrievedPostList, (_state, { posts }) => { return { posts: [...posts] } })
+    on(PostApiActions.retrievedPostList, (_state, posts) => { return [...posts.posts] })
 );
 
 export const activePostReducer = createReducer(
