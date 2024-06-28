b7075fc7c2f752514dd6d62ecd53337fc7613338
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-06-28 10:49:19 +0100

    implement straightforward approach

--- src/app/components/all-posts/all-posts.component.ts
+++ src/app/components/all-posts/all-posts.component.ts
@@ -5,16 +5,19 @@ import { PostApiActions } from '../../state/actions/posts.actions';
 import { Post } from '../../state/models/post';
 import { selectPosts } from '../../state/selectors/posts.selectors';
 import { CommonModule } from '@angular/common';
+import { PostIdComponent } from '../post-id/post-id.component';
 
 @Component({
   selector: 'app-all-posts',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PostIdComponent],
   templateUrl: './all-posts.component.html',
   styleUrl: './all-posts.component.scss',
   providers: [NetworkService]
 })
 export class AllPostsComponent implements OnInit {
+
+  public cells = Array(10).fill(0);
 
   constructor(private __network: NetworkService, private store: Store) { }
 
--- src/app/components/post-id/post-id.component.ts
+++ src/app/components/post-id/post-id.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Signal, WritableSignal, computed, signal } from '@angular/core';
+import { Post } from '../../state/models/post';
+import { post_props } from '../../constants/constants';
 
 @Component({
   selector: 'app-post-id',
@@ -9,4 +11,20 @@ import { Component } from '@angular/core';
 })
 export class PostIdComponent {
 
+  @Input() post: Post = {};
+  public activePropIdx: number = 0;
+
+
+  // naive approach - causes too many renders. App will get laggy for larger datasets
+  // getDisplayProp() {
+  //   return this.post[post_props[this.activePropIdx] as keyof Post]
+  // }
+
+  // getActiveProp() {
+  //   return post_props[this.activePropIdx]
+  // }
+
+  // updateDisplayProp() {
+  //   this.activePropIdx = (this.activePropIdx < post_props.length - 1) ? this.activePropIdx + 1 : 0;
+  // }
 }
