b056879a9356e4c54e982b159a7c1c3a7e3732b8
Sahil Naikdhure <sanaikdhure@gmail.com>
2024-06-28 22:01:02 +0100

    add tests: network service, app component, all-posts component

--- src/app/components/all-posts/all-posts.component.spec.ts
+++ src/app/components/all-posts/all-posts.component.spec.ts
@@ -1,23 +1,89 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { NetworkService } from '../../services/network.service';
 import { AllPostsComponent } from './all-posts.component';
+import { PostApiActions } from '../../state/actions/posts.actions';
+import { selectActivePost, selectPosts } from '../../state/selectors/posts.selectors';
+import { of } from 'rxjs';
+import { Post } from '../../state/models/post';
+import { HttpClientTestingModule, provideHttpClientTesting } from '@angular/common/http/testing';
+import { CommonModule } from '@angular/common';
+import { PostIdComponent } from '../post-id/post-id.component';
+import { provideHttpClient } from '@angular/common/http';
+import { Action } from '@ngrx/store';
 
 describe('AllPostsComponent', () => {
   let component: AllPostsComponent;
   let fixture: ComponentFixture<AllPostsComponent>;
+  let store: MockStore;
+  let networkService: jasmine.SpyObj<NetworkService>;
+  const initialState = { posts: [], activePost: -1 };
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AllPostsComponent]
-    })
-    .compileComponents();
+    const networkServiceSpy = jasmine.createSpyObj('NetworkService', ['getAllPosts']);
 
+    await TestBed.configureTestingModule({
+      imports: [CommonModule, HttpClientTestingModule, AllPostsComponent, PostIdComponent],
+      providers: [
+        provideMockStore({ initialState }),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: NetworkService, useValue: networkServiceSpy }
+      ]
+    }).compileComponents();
+
+    store = TestBed.inject(MockStore);
+    networkService = TestBed.inject(NetworkService) as jasmine.SpyObj<NetworkService>;
     fixture = TestBed.createComponent(AllPostsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have default activePostId of -1', () => {
+    expect(component.activePostId).toBe(-1);
+  });
+
+  it('should select active post from store', () => {
+    const mockPostId = -1;
+    store.overrideSelector(selectActivePost, mockPostId);
+    store.refreshState();
+    fixture.detectChanges();
+
+    expect(component.activePostId).toBe(mockPostId);
+  });
+
+  it('should fetch posts and store posts', () => {
+    const mockPosts: Post[] = [
+      { id: 1, title: 'Post 1', body: 'Body 1', userId: 1 },
+      { id: 2, title: 'Post 2', body: 'Body 2', userId: 1 }
+    ];
+    networkService.getAllPosts.and.returnValue(of(mockPosts));
+
+    component.ngOnInit();
+    store.overrideSelector(selectPosts, mockPosts);
+    store.refreshState();
+    store.select(selectPosts).subscribe(posts => {
+      expect(posts.length).toBe(mockPosts.length);
+    })
+    const actions: Action[] = [];
+    store.scannedActions$.subscribe(action => actions.push(action));
+    expect(actions.length).toBeGreaterThan(0);
+  });
+
+  it('should render posts', () => {
+    const mockPosts: Post[] = [
+      { id: 1, title: 'Post 1', body: 'Body 1' },
+      { id: 2, title: 'Post 2', body: 'Body 2' }
+    ];
+    store.overrideSelector(selectPosts, mockPosts);
+    store.refreshState();
+    fixture.detectChanges();
+    const postElements = fixture.nativeElement.querySelectorAll('#postRender')[0].children;
+    expect(postElements.length).toBe(mockPosts.length);
+    expect(postElements[0].textContent).toContain('Post 1');
+    expect(postElements[1].textContent).toContain('Post 2');
   });
 });
--- src/app/services/network.service.spec.ts
+++ src/app/services/network.service.spec.ts
@@ -1,16 +1,34 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 import { NetworkService } from './network.service';
+import { Post } from '../state/models/post';
 
 describe('NetworkService', () => {
   let service: NetworkService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(NetworkService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()]
+    });
+    service = TestBed.inject(NetworkService);
+  })
+
+  it('should fetch all posts', () => {
+    const httpTesting = TestBed.inject(HttpTestingController);
+    const dummyPosts: Post[] = [
+      { id: 1, title: 'Post 1', body: 'Body 1', userId: 1 },
+      { id: 2, title: 'Post 2', body: 'Body 2', userId: 2 }
+    ];
+
+    service.getAllPosts().subscribe(posts => {
+      expect(posts.length).toBe(2);
+      expect(posts).toEqual(dummyPosts);
+    });
+    const req = httpTesting.expectOne('https://jsonplaceholder.typicode.com/posts', 'Request to fetch posts');
+    expect(req.request.method).toBe('GET');
+    req.flush(dummyPosts); // Respond with the dummy posts
+    httpTesting.verify();
+  })
 });
